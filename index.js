ad05361f983eb20381aa9bfec0386a702722de63
Ruby pal <rubyp20001@gmail.com>
2023-01-18 15:05:26 +0000

    abc

--- index.js
+++ index.js
@@ -19,22 +19,4 @@ app.post('/add', (req, res) => {
   // res.sendFile(path.join(__dirname + '/main.html'));
 });
 module.exports = app;
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+
